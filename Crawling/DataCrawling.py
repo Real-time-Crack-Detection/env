67af17c552979fa1a3daa45d82b69667ed3cf9e7
Hyun Woong Choi <52322529+hwchoi96@users.noreply.github.com>
2020-03-06 15:49:24 +0900

    Update DataCrawling.py

--- Crawling/DataCrawling.py
+++ Crawling/DataCrawling.py
@@ -15,13 +15,16 @@ browser = webdriver.Chrome('chromedriver.exe')
 # Need to Chrome driver - https://chromedriver.chromium.org/downloads
 count = 0
 
-keyword = 'crack' # search keyword
-
+keyword = 'cracks' # search keyword
+page_down_count = 120
 
 photo_list = []
 result = []
 url = "https://www.google.co.in/search?q=" + keyword + "&source=lnms&tbm=isch"
 
+
+if not os.path.exists(DEFAULT_PATH):
+    os.mkdir(DEFAULT_PATH)
 if not os.path.exists(DEFAULT_PATH + '/' + keyword):
     os.mkdir(DEFAULT_PATH + '/' + keyword)
 
@@ -29,8 +32,22 @@ if not os.path.exists(DEFAULT_PATH + '/' + keyword):
 browser.get(url)
 time.sleep(1)
 
+
+body = browser.find_element_by_tag_name('body')
+
+for i in range(0, page_down_count):
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.1)
+
+    # if browser.find_element_by_class_name("mye4qd") is not None:
+    #     browser.find_element_by_class_name("mye4qd").click()
+    #     time.sleep(2)
+
+
+
 photo_list = browser.find_elements_by_tag_name("img.rg_i")
 
+print('Find object : ', len(photo_list))
 index = 0
 for i in range(0, len(photo_list)):
 
@@ -57,19 +74,18 @@ for i in range(0, len(photo_list)):
         print(e)
 
 
-    if index < 500:
-        filename ="{}/{}/{}{}{}".format(DEFAULT_PATH, keyword,keyword, str(index), ".jpg")
-        with open(filename, "wb") as f:
-            f.write(t)
-            index += 1
-            current_src = ""
-            print("Img Save Success")
-    else:
-        browser.close()
-        break
+    filename ="{}/{}/{}{}{}".format(DEFAULT_PATH, keyword,keyword, str(index), ".jpg")
+    with open(filename, "wb") as f:
+        f.write(t)
+        index += 1
+        current_src = ""
+        print("Img Save Success")
     index += 1
 
     exit_button = browser.find_element_by_css_selector('#Sva75c > div > div > div:nth-child(2) > a > div > svg > polygon')
     exit_button.click()
 
     time.sleep(1)
+
+print('Dataset Crawling complete..')
+browser.close()
